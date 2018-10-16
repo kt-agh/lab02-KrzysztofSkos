964444be936420e54d6f5694b775e127151b248c
Krzysztof Skos <krzysztof.skos@gmail.com>
2018-10-16 14:19:09 +0200

    Poprawiona ortografia

--- Lab2.cc
+++ Lab2.cc
@@ -2,12 +2,15 @@
 #include <iostream>
 using namespace std;
 
-//To jest komentaż do tego pliku. Ten plik jest ewidentnie źle fsormatowany i należy to koniecznie poprawić, pamiętając o nazwach zmiennych
-//dopuszczalnej długości linii, znakach z kodu ANSI, wcięciach i dbalości o wsykoiehj jakosci kometarze
+//To jest komentaz do tego pliku. Ten plik jest ewidentnie zle 
+//sformatowany i nalezy to koniecznie poprawic, pamietajac o nazwach zmiennych,
+//dopuszczalnej długości linii, znakach z kodu ANSI,
+//wcieciach i dbalosci o wysokoiej jakosci kometarze.
 
-int main() { int PierwszaZmienna=5; int Drugazmienna=7;
-int WynikDziałania=PierwszaZmienna+Drugazmienna;
-
-cout <<"Wynik Dodawania Dwóch Liczb To:"<<WynikDziałania<<endl; // Wypisuje wynik dzialania
-return 0;
+int main() { 
+ int PierwszaZmienna = 5; 
+ int Drugazmienna = 7;
+ int WynikDzialania = PierwszaZmienna + Drugazmienna;
+ cout <<"Wynik Dodawania Dwoch Liczb To:"<<WynikDzialania<<endl; // Wypisuje wynik dzialania.
+ return 0;
 }
